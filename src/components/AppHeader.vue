19c14abf510d45b10a6d16281006eb786f448975
RodiaGiordano <rodiagiordano93@gmail.com>
2023-09-14 14:57:30 +0200

    milestone 0

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+import CardList from "./CardList.vue";
+import SearchBar from "./SearchBar.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  components: { CardList, SearchBar },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,19 @@
+<script>
+import Card from "./Card.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  components: { Card },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
